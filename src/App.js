17ab65c03652f88acd04b632a20d90114cb42d2e
Precious Udegbue <Precious.Udegbue@sterling.ng>
2022-07-24 22:34:02 +0100

    added carousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Navbar from './components/Navbar';
 import Featured from './components/Featured/Featured';
 import Houses from './components/Houses/Houses';
+import Carousel from './components/Carousel/Carousel';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Featured />
       <Houses />
+      <Carousel />
+      <Featured />
     </div>
   );
 }
---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './carousel.css';
+
+const Carousel = () => {
+  return (
+    <div className='carousel'>
+        <div>
+
+        </div>
+        <div className='carousel__div second'></div>
+        <div className='carousel__div'></div>
+        <div className='carousel__div'></div>
+    </div>
+  )
+}
+
+export default Carousel
