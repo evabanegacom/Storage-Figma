cee58ff3b3fd7e961f9aa845e1de698791b4a5a6
Precious Udegbue <Precious.Udegbue@sterling.ng>
2022-07-27 22:35:47 +0100

    completed footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Box, Grid } from '@mui/material';
 import './footer.css';
 import blueStorage from '../../assets/blueStorage.svg';
+import socialOne from '../../assets/Group 9.svg';
+import shape from '../../assets/Shape.svg';
+import { css } from '@emotion/css';
 
 const Footer = () => {
   return (
@@ -17,33 +20,43 @@ const Footer = () => {
              Nam posuere accumsan porta. Integer id orci sed ante 
              tincidunt tincidunt sit amet sed libero.
              </div>
-             <div>
+             <div className='footer__storageConnect-one-copy-right'>
              © StorageConnect 2022, All Rights Reserved
              </div>
            </div>
         </Grid>
         <Grid item xs={12} sm={6} md={4} lg={3}>
            <div className='footer__storageConnect'>
-             <div>Quick Links</div>
-             <div>Active Listings</div>
-             <div>Self your Self Storage</div>
-             <div>Free Evaluation</div>
-             <div>Learn</div>
+             <div className='footer__storageConnect-header'>Quick Links</div>
+             <div className='footer__storageConnect-active'>Active Listings</div>
+             <div className='footer__storageConnect-active'>Self your Self Storage</div>
+             <div className='footer__storageConnect-active'>Free Evaluation</div>
+             <div className='footer__storageConnect-active'>Learn</div>
            </div>
         </Grid>
         <Grid item xs={12} sm={6} md={4} lg={3}>
            <div className='footer__storageConnect'>
-             <div>News Letter</div>
-             <div>Get latest updates right in your inbox</div>
-             <div><input placeholder='Enter your Email'/></div>
-             <div><button>Subscribe Now</button></div>
+             <div className='footer__storageConnect-header'>News Letter</div>
+             <div className='footer__storageConnect-active'>Get latest updates right in your inbox</div>
+             <div className='footer__storageConnect-input'><input placeholder='Enter your Email'/></div>
+             <div className='footer__storageConnect-button'><button>Subscribe Now</button></div>
            </div>
         </Grid>
         <Grid item xs={12} sm={6} md={4} lg={3}>
            <div className='footer__storageConnect'>
-             <div>1</div>
-             <div>2</div>
-             <div>3</div>
+             <div className='footer__storageConnect-header'>Let's Get Social</div>
+             <div className='footer__storageConnect-socials'>
+                <img src={socialOne} alt='facebook' />
+                <img className={footerStorageImg } src={socialOne} alt='facebook' />
+                <img className={footerStorageImg } src={socialOne} alt='facebook' />
+                <img  className={footerStorageImg } src={socialOne} alt='facebook' />
+             </div>
+             <div className='footer__storageConnect-drop-down'>
+                <select>
+                    <option value=''>&#127760;
+                        English-En</option>
+                </select>
+             </div>
            </div>
         </Grid>
       </Grid>
@@ -51,4 +64,7 @@ const Footer = () => {
   )
 }
 
+const footerStorageImg = css`
+  margin-left: 10px;
+  `;
 export default Footer
